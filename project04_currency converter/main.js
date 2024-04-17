b295c5b9ed3ed0297f36c68fbbd6da9c76163df1
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-04-17 23:22:50 +0500

    Add files via upload

---
+++ project04_currency converter/main.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.93,
+    GBP: 0.8,
+    INR: 83.57,
+    PKR: 278,
+    SAR: 3.75,
+    KWD: 0.31,
+    BHD: 0.38,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Select From Currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "SAR", "KWD", "BHD"]
+    },
+    {
+        name: "to",
+        message: "Select To Currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "SAR", "KWD", "BHD"]
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount",
+        type: "number"
+    }
+]);
+let userFromCurrency = user_answer.from;
+let userToCurrency = user_answer.to;
+let fromAmount = currency[userFromCurrency]; // exchange rate
+let toAmount = currency[userToCurrency]; // exchange rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD base currency // 4
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
